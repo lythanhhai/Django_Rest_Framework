b3ffa895803321c1c88e63d4afe9a357dcb8fb65
lythanhhai <lythanhhait@gmail.com>
2022-04-21 14:20:25 +0700

    test continuos

---
+++ frontend/src/Component/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../Asset/Navbar/Navbar.scss'
+
+const Navbar = () => {
+    return(
+        <>
+            <p className='Title'>Django React App</p>
+            <section className='Navbar'>
+                <Link className='Navbar__Home' to="">Home</Link>
+                <Link className='Navbar__Department' to="">Department</Link>
+                <Link className='Navbar__Employee' to="">Employee</Link>
+            </section>
+        </>
+    );
+}
+ 
+export default React.memo(Navbar)
